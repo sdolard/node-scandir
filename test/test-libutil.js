5a8edbac9d03d0b21ac704a38034cb016a62f195
Sébasten Dolard <sdolard@gmail.com>
2013-09-28 10:42:24 +0200

    mv plugin

--- test/test-libutil.js
+++ test/test-libutil.js
@@ -12,7 +12,6 @@ describe('libutil', function(){
 		it('should convert 1011', function(){
 			assert.equal(libutil.msToHuman(1011), '0m1.011s');
 		});
-
 		it('should convert 1111', function(){
 			assert.equal(libutil.msToHuman(1111), '0m1.111s');
 		});
